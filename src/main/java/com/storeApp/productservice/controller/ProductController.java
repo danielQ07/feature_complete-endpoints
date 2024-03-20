47b91a4bfae7f50fbec3c87adf49165d8fb50275
Daniel Q <danqp0712@gmail.com>
2024-03-20 11:15:35 -0600

    cambios

---
+++ src/main/java/com/storeApp/productservice/controller/ProductController.java
@@ -0,0 +1,14 @@
+package com.storeApp.productservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class ProductController {
+    @GetMapping
+    public String holamUndo(){
+        return "Hola Mundo";
+    }
+}
