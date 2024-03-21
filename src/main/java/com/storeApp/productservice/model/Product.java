a3b4575a5c05bddc32482b28171fe0ffd1692fbf
Daniel Q <danqp0712@gmail.com>
2024-03-20 21:14:24 -0600

    VERSION nueva

--- src/main/java/com/storeApp/productservice/model/Product.java
+++ src/main/java/com/storeApp/productservice/model/Product.java
@@ -1,9 +1,22 @@
 package com.storeApp.productservice.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
-
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(
+        uniqueConstraints = @UniqueConstraint(
+           name = "unique_productCode",
+           columnNames = "productCode"
+        )
+)
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
