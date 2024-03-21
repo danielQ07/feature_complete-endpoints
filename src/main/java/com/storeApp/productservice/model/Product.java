3ea99227fa0bb1d5673d3938df3c374806d36e76
Daniel Q <danqp0712@gmail.com>
2024-03-20 20:41:28 -0600

    VERSION ESTABLE

--- src/main/java/com/storeApp/productservice/model/Product.java
+++ src/main/java/com/storeApp/productservice/model/Product.java
@@ -1,4 +1,15 @@
 package com.storeApp.productservice.model;
 
+import jakarta.persistence.*;
+
+@Entity
+
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+    private String productCode;
+    private String description;
+    private Integer quantity;
+    private Double price;
 }
